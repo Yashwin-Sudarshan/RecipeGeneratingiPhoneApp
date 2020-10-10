f11ca4c4676323bd7b5f03d8d0d0c0dbc58dad71
Alexander <s3781797@student.rmit.edu.au>
2020-10-10 18:54:19 +1100

    UnitTest Bundle added

---
+++ RecipeSolved/RecipeSolvedUnitTests/RecipeSolvedUnitTests.swift
@@ -0,0 +1,33 @@
+//
+//  RecipeSolvedUnitTests.swift
+//  RecipeSolvedUnitTests
+//
+//  Created by Alexander LoMoro on 10/10/20.
+//  Copyright © 2020 Alexander LoMoro. All rights reserved.
+//
+
+import XCTest
+@testable import RecipeSolved
+
+class RecipeSolvedUnitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testGreetingString() {
+        
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
