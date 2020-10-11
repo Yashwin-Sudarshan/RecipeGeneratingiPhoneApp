4691a348707bfdb81b612886f9ba3636ebb309d9
Alexander <s3781797@student.rmit.edu.au>
2020-10-11 14:22:46 +1100

    4 Unit Tests with code commenting

--- RecipeSolved/RecipeSolvedUnitTests/RecipeSolvedUnitTests.swift
+++ RecipeSolved/RecipeSolvedUnitTests/RecipeSolvedUnitTests.swift
@@ -20,6 +20,7 @@ class RecipeSolvedUnitTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
+    // Unit test 1: Check weather label updates based on data that is passed to it
     func testWeatherUpdating() {
         var currentTemp = 0     // Set temp to 0
         var tempLabel = "\(currentTemp)ºc"      // Update temp label with the same style logic in ViewController
@@ -37,55 +38,55 @@ class RecipeSolvedUnitTests: XCTestCase {
         XCTAssertEqual(tempLabel, "18ºc")
     }
 
-    func testGreetingString() {
-        // Test with hour set to 1
-        var hour = 1
+    // Unit test 2: Check Morning is displayed when time is in AM
+    func testGreetingStringMorning() {
+        // Test with hour set to 0..11
         var text = ""
         
-        switch hour {
-        case 0..<12 : text = "Morning"       // Show morning if time is AM
-        case 12..<18 : text = "Afternoon"    // Show afternoon if time is between 12PM and 6PM
-        case 18..<24 : text = "Evening"    // Show evening if time is after 6PM
-        default: text = "Day"                // Default to Day if time of day cannot be found
+        for i in 0...11 {
+            switch i {
+            case 0..<12 : text = "Morning"       // Show morning if time is AM
+            case 12..<18 : text = "Afternoon"    // Show afternoon if time is between 12PM and 6PM
+            case 18..<24 : text = "Evening"    // Show evening if time is after 6PM
+            default: text = "Day"                // Default to Day if time of day cannot be found
+            }
+            
+            XCTAssertEqual(text, "Morning")
         }
+    }
+    
+    // Unit test 3: Check Evening is displayed when time is in betwen 12PM & 5PM
+    func testGreetingStringAfternoon() {
+        // Test with hour set from 12..<18
+        var text = ""
         
-        XCTAssertEqual(text, "Morning")
-        
-        // Test with hour set to 13
-        hour = 13
-        
-        switch hour {
-        case 0..<12 : text = "Morning"       // Show morning if time is AM
-        case 12..<18 : text = "Afternoon"    // Show afternoon if time is between 12PM and 6PM
-        case 18..<24 : text = "Evening"    // Show evening if time is after 6PM
-        default: text = "Day"                // Default to Day if time of day cannot be found
+        for i in 12...17 {
+            switch i {
+            case 0..<12 : text = "Morning"       // Show morning if time is AM
+            case 12..<18 : text = "Afternoon"    // Show afternoon if time is between 12PM and 6PM
+            case 18..<24 : text = "Evening"    // Show evening if time is after 6PM
+            default: text = "Day"                // Default to Day if time of day cannot be found
+            }
+            
+            XCTAssertEqual(text, "Afternoon")
         }
+    }
+    
+    // Unit test 4: Check Evening is displayed when time is in betwen 5PM & 11:59PM
+    func testGreetingStringEvening() {
+        // Test with hour set from 18..<24
+        var text = ""
         
-        XCTAssertEqual(text, "Afternoon")
-        
-        // Test with hour set to 19
-        hour = 19
-        
-        switch hour {
-        case 0..<12 : text = "Morning"       // Show morning if time is AM
-        case 12..<18 : text = "Afternoon"    // Show afternoon if time is between 12PM and 6PM
-        case 18..<24 : text = "Evening"    // Show evening if time is after 6PM
-        default: text = "Day"                // Default to Day if time of day cannot be found
+        for i in 18...23 {
+            switch i {
+            case 0..<12 : text = "Morning"       // Show morning if time is AM
+            case 12..<18 : text = "Afternoon"    // Show afternoon if time is between 12PM and 6PM
+            case 18..<24 : text = "Evening"    // Show evening if time is after 6PM
+            default: text = "Day"                // Default to Day if time of day cannot be found
+            }
+            
+            XCTAssertEqual(text, "Evening")
         }
-        
-        XCTAssertEqual(text, "Evening")
-        
-        // Test with hour set to 25 (outside of scope) should go default
-        hour = 25
-        
-        switch hour {
-        case 0..<12 : text = "Morning"       // Show morning if time is AM
-        case 12..<18 : text = "Afternoon"    // Show afternoon if time is between 12PM and 6PM
-        case 18..<24 : text = "Evening"    // Show evening if time is after 6PM
-        default: text = "Day"                // Default to Day if time of day cannot be found
-        }
-        
-        XCTAssertEqual(text, "Day")
     }
 
     func testPerformanceExample() {
